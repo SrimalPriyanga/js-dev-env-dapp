5b0b0d11068f1faebecb1ad6a7ab29288bf421c7
Srimal Priyanga <s.priyanga22@gmail.com>
2020-04-19 12:00:54 +0530

    add sample test cases to configure testing framework

---
+++ buildScripts/testSetup.js
@@ -0,0 +1,7 @@
+// Not a transpiled file, so must use CommOnJS and ES5
+
+// Disable webpack features that Mocha doesn't understand
+// require.extensions(".css") = function () {};
+
+// Register babel to transpile before test run
+require("babel-register")();
