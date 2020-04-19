2abfd8afdcb7c8fd6e4065b392278c36436b1825
Srimal Priyanga <s.priyanga22@gmail.com>
2020-04-19 18:38:50 +0530

    use standerd fetch api for http

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -15,6 +15,14 @@ app.use(
   })
 );
 
+app.get("/users", function (req, res) {
+  res.json([
+    { id: 1, fname: "bob1", lname: "sm1" },
+    { id: 2, fname: "bob2", lname: "sm2" },
+    { id: 3, fname: "bob3", lname: "sm3" },
+  ]);
+});
+
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "../src/index.html"));
 });
--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-import "./index.css";
-import numeral from "numeral";
+import {getUsers} from './api/userApi';
 
-const courseValue = numeral(1000).format("$0,0.00");
-console.log(`Value ${courseValue} !`);
+getUsers().then((result) => {
+  let userBody = "";
 
-// debugger;
+  result.forEach(user => {
+    userBody += `
+      <tr>
+        <td><a href="#" data-id='${user.id}' class='deleteUser'>Delete</a></td>
+        <td>${user.id}</td>
+        <td>${user.fname}</td>
+        <td>${user.lname}</td>
+      </tr>
+      `
+  });
+
+  global.document.getElementById('users').innerHTML = userBody;
+});
