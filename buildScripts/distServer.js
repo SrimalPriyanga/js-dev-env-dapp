caf6cfdc8fb2fc5e17473b0e04ee1f92f0bcd445
Srimal Priyanga <s.priyanga22@gmail.com>
2020-04-21 09:33:53 +0530

    prod webpack minification, sourcemap, toggle mock api, local dist server, dynamicHTML, bundle splitting. npm scripts

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -1,19 +1,16 @@
 import express from "express";
 import path from "path";
 import open from "open";
-import webpack from "webpack";
-import config from "../webpack.config.dev";
-
+import compression from "compression";
 const port = 3000;
 const app = express();
-const compiler = webpack(config);
 
-app.use(
-  require("webpack-dev-middleware")(compiler, {
-    noInfo: true,
-    publicPath: config.output.publicPath,
-  })
-);
+app.use(express.static("dist"));
+app.use(compression());
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
+});
 
 app.get("/users", function (req, res) {
   res.json([
@@ -21,10 +18,6 @@ app.get("/users", function (req, res) {
     { id: 2, firstName: "bob2", lastName: "sm2" },
     { id: 3, firstName: "bob3", lastName: "sm3" },
   ]);
-});
-
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "../src/index.html"));
 });
 
 app.listen(port, function (err) {
