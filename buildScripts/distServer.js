0bbf566cee934a993d583917f3188e487c24c817
Srimal Priyanga <s.priyanga22@gmail.com>
2020-05-02 12:36:21 +0530

    add base host api url & change distServer

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -12,14 +12,6 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
-app.get("/users", function (req, res) {
-  res.json([
-    { id: 1, firstName: "bob1", lastName: "sm1" },
-    { id: 2, firstName: "bob2", lastName: "sm2" },
-    { id: 3, firstName: "bob3", lastName: "sm3" },
-  ]);
-});
-
 app.listen(port, function (err) {
   if (err) {
     // eslint-disable-next-line no-console
